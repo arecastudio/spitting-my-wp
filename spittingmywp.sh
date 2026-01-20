2ae12822dcbad6a0d6af7762868e8d25d16a066d
arecastudio <ondiisrail@gmail.com>
2026-01-20 14:41:19 +0900

    Ollama AI command added

--- spittingmywp.sh
+++ spittingmywp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source .env
 #echo 'hello world'
 
 #filter out content
@@ -7,18 +8,19 @@
 #jubi=$(rss2json 'https://jubi.id/feed'|jq -r .''|gron|grep -E 'items\[[0-9]{1,}\]\.link\ =')
 #rss2json 'https://jubi.id/feed' > jubi.json
 jubi=$(cat ./jubi.json|gron|grep -E '\.items\[[0-9]{1,}\]\.'|grep -E '\.title|\.guid|\.link|\.published|\.content'|gron -u)
-
-jq -c '.items[]' ./jubi.json|while read -r item;do
-guid_=$(echo "$item"|jq -c '.guid'|sed 's/\"//g'|md5sum|awk '{print $1}')
-title_=$(echo "$item"|jq -c '.title'|sed 's/\"//g')
-link_=$(echo "$item"|jq -c '.link'|sed 's/\"//g')
-published_=$(echo "$item"|jq -c '.publishedParsed'|sed 's/\"//g'|grep -Eo '[0-9]{4}\-[0-9]{2}\-[0-9]{2}')
-content_=$(echo "$item"|jq -c '.content'|sed 's/\"//g'|sed 's/<[^>]*>//g;s/&#[0-9]\{4\};//g;s/\\n\|&lt;\|&gt;\|\/p//g;s/[jJ]ubi\.id//g;s/[jJ]ubi//g')
-photo_=$(curl -q -s $link_|grep -Eo -m1 'https://jubi.id/wp-content/uploads/[0-9]{4}/[0-9]{2}/.*\.[a-zA-Z0-9]{2,}')
-echo $guid_
-echo $title_
-echo $link_
-echo $published_
-echo $photo_
-echo $content_
+#echo $AI_PROMPT
+jq -c '.items[]' ./jubi.json|while read -r item
+do
+	guid_=$(echo "$item"|jq -c '.guid'|sed 's/\"//g'|md5sum|awk '{print $1}')
+	title_=$(echo "$item"|jq -c '.title'|sed 's/\"//g')
+	link_=$(echo "$item"|jq -c '.link'|sed 's/\"//g')
+	published_=$(echo "$item"|jq -c '.publishedParsed'|sed 's/\"//g'|grep -Eo '[0-9]{4}\-[0-9]{2}\-[0-9]{2}')
+	content_=$(echo "$item"|jq -c '.content'|sed 's/\"//g'|sed 's/<[^>]*>//g;s/&#[0-9]\{4\};//g;s/\\n\|&lt;\|&gt;\|\/p//g;s/[jJ]ubi\.id//g;s/[jJ]ubi//g')
+	photo_=$(curl -q -s $link_|grep -Eo -m1 'https://jubi.id/wp-content/uploads/[0-9]{4}/[0-9]{2}/.*\.[a-zA-Z0-9]{2,}')
+	echo $guid_
+	echo $title_
+	echo $link_
+	echo $published_
+	echo $photo_
+	echo "Berita:[$content_] . Sumber:[$link_ - $published_]"|ollama run gemma3:4b "$AI_PROMPT" 2>/dev/null|tee -a ./tmps/news-generated-$RANDOM.txt
 done
